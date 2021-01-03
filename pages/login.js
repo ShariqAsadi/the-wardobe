01cb3be618adee3cf63166929d8f7d66c82bf69e
Shariq Asadi <shariq.asadi@gmail.com>
2021-01-04 01:00:37 +0500

    Setting up firebase with next

---
+++ pages/login.js
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import { useAuth } from '../utils/useAuth';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { signinWithEmail, signupWithEmail } = useAuth();
+
+  const handleSignin = e => {
+    e.preventDefault();
+    console.log('coems here?');
+    signinWithEmail(email, password);
+  };
+
+  const handleSignup = e => {
+    e.preventDefault();
+    console.log('coems here?');
+    signupWithEmail(email, password);
+  };
+
+  return (
+    <>
+      <div>Signin</div>
+      <form onSubmit={handleSignin}>
+        <input
+          type='text'
+          name='email'
+          value={email}
+          placeholder='email'
+          onChange={e => setEmail(e.target.value)}
+        />
+        <input
+          type='text'
+          name='password'
+          value={password}
+          placeholder='password'
+          onChange={e => setPassword(e.target.value)}
+        />
+      </form>
+      <button onClick={handleSignin}>Submit</button>
+
+      <div>Signup</div>
+      <form onSubmit={handleSignup}>
+        <input
+          type='text'
+          name='email'
+          value={email}
+          placeholder='email'
+          onChange={e => setEmail(e.target.value)}
+        />
+        <input
+          type='text'
+          name='password'
+          value={password}
+          placeholder='password'
+          onChange={e => setPassword(e.target.value)}
+        />
+      </form>
+      <button onClick={handleSignup}>Submit</button>
+    </>
+  );
+}
---
+++ utils/useAuth.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect, useContext, createContext } from 'react';
+import Router from 'next/router';
+import nookies from 'nookies';
+import firebase from '../firebase/firebaseClient';
+
+const AuthContext = createContext();
+
+const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onIdTokenChanged(async rawUser => {
+      if (!rawUser) {
+        setUser(null);
+        nookies.set(undefined, 'token', '', {});
+      } else {
+        const token = rawUser.getIdToken();
+        setUser(rawUser);
+        nookies.set(undefined, 'token', token, {});
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const signinWithEmail = (email, password) => {
+    // setLoading(true);
+    return firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(response => {
+        console.log(response.user);
+        // handleUser(response.user);
+        Router.push('/');
+      });
+  };
+
+  const signupWithEmail = (email, password) => {
+    // setLoading(true);
+    return firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(response => {
+        console.log(response.user);
+        // handleUser(response.user);
+        Router.push('/');
+      });
+  };
+
+  const signinWithGoogle = redirect => {
+    // setLoading(true);
+    return firebase
+      .auth()
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then(response => {
+        // handleUser(response.user);
+        // if (redirect) {
+        //   Router.push(redirect);
+        // }
+        Router.push('/');
+      });
+  };
+
+  const signout = () => {
+    Router.push('/');
+
+    return firebase.auth().signOut();
+    // .then(() => handleUser(false));
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, signinWithEmail, signinWithGoogle, signupWithEmail, signout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export { useAuth, AuthProvider };
